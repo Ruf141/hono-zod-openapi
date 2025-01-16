0c18b4b1118ced1cebb87873c3d58dcf6ff501be
Ruf141 <mad0908141@gmail.com>
2025-01-16 16:32:41 +0900

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { app as todoRoute } from "./routes/todo";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    ok: true,
+    message: "Hello Hono",
+  });
+});
 
-export default app
+app.route("/todo", todoRoute);
+
+export default app;
