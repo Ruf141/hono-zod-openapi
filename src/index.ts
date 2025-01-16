db99bce4cb14eb992826e636ef9d5ce6db8e5225
Ruf141 <mad0908141@gmail.com>
2025-01-16 23:24:08 +0900

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { app as todoRoute } from "./routes/todo";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new OpenAPIHono();
 
@@ -11,5 +12,15 @@ app.get("/", (c) => {
 });
 
 app.route("/todo", todoRoute);
+
+app.doc("/doc",{
+  openapi:"3.0.0",
+  info:{
+    version:"1.0.0",
+    title:"My TODO API with Hono"
+  }
+})
+
+app.get("/ui", swaggerUI({url:"/doc"}))
 
 export default app;
